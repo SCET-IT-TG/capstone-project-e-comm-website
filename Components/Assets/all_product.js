10945542789379d38c4c1fa15cbcc8a2028d1f3e
khyatisingh03 <155899940+khyatisingh03@users.noreply.github.com>
2024-04-08 11:05:19 +0530

    Add files via upload

---
+++ Components/Assets/all_product.js
@@ -0,0 +1,325 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.png";
+import p19_img from "./product_19.png";
+import p20_img from "./product_20.png";
+import p21_img from "./product_21.png";
+import p22_img from "./product_22.png";
+import p23_img from "./product_23.png";
+import p24_img from "./product_24.png";
+import p25_img from "./product_25.png";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p28_img from "./product_28.png";
+import p29_img from "./product_29.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
+import p32_img from "./product_32.png";
+import p33_img from "./product_33.png";
+import p34_img from "./product_34.png";
+import p35_img from "./product_35.png";
+import p36_img from "./product_36.png";
+import p37_img from "./product_37.png";
+import p38_img from "./product_38.png";
+import p39_img from "./product_39.png";
+import p40_img from "./product_40.png";
+
+let all_product =[
+    {
+        id: 1,
+        name: "Pink Tradescantia Nanouk",
+        category: "plants",
+        image: p1_img,
+        price: 900,
+    },
+    {
+        id: 2,
+        name: "Swiss Cheese Vine",
+        category: "plants",
+        image: p2_img,
+        price: 700,
+    },
+    {
+        id: 3,
+        name: "Spider Plant",
+        category: "plants",
+        image: p3_img,
+        price: 1200,
+    },
+    {
+        id: 4,
+        name: "Ficus Robusta",
+        category: "plants",
+        image: p4_img,
+        price: 1000,
+    },
+    {
+        id: 5,
+        name: "Small Aloe Vera",
+        category: "plants",
+        image: p5_img,        
+        price: 400,
+    },
+    {
+        id: 6,
+        name: "Syngonium Red Arrow",
+        category: "plants",
+        image: p6_img,
+        price: 1900,
+    },
+    {
+        id: 7,
+        name: "Hildewintera Cristata",
+        category: "plants",
+        image: p7_img,
+        price: 2000,
+    },
+    {
+        id: 8,
+        name: "Caladium Sweethearts",
+        category: "plants",
+        image: p8_img,
+        price: 2200,
+    },
+    {
+        id: 9,
+        name: "Blue Hand Crafted 18in Wooden Mirror",
+        category: "mirror",
+        image: p9_img,
+        price: 1198,
+    },
+    {
+        id: 10,
+        name: "D'DASS Peacock Wall Mirror",
+        category: "mirror",
+        image: p10_img,
+        price: 1109,
+    },
+    {
+        id: 11,
+        name: "Round Wall Mirror in White Colour",
+        category: "mirror",
+        image: p11_img,
+        price: 2089,
+    },
+    {
+        id: 12,
+        name: "Blue Cotton Fine Decorative Mirror",
+        category: "mirror",
+        image: p12_img,
+        price: 849,
+    },
+    {
+        id: 13,
+        name: "Blue & White Glass Decorative Mirror",
+        category: "mirror",
+        image: p13_img,
+        price: 3200,
+    },
+    {
+        id: 14,
+        name: "Pink Cotton Imperial Decorative Mirror",
+        category: "mirror",
+        image: p14_img,
+        price: 849,
+    },
+    {
+        id: 15,
+        name: "Handmade Gold Metal Framed Wall Mirror",
+        category: "mirror",
+        image: p15_img,
+        price: 2859,
+    },
+    {
+        id: 16,
+        name: "Silver Antique Mirror",
+        category: "mirror",
+        image: p16_img,
+        price: 4089,
+    },
+    {
+        id: 17,
+        name: "37 Inches Shub Labh Toran",
+        category: "festive",
+        image: p17_img,
+        price: 749,
+    },
+    {
+        id: 18,
+        name: "Floral rangoli mat, Indian Decor",
+        category: "festive",
+        image: p18_img,
+        price: 1199,
+    },
+    {
+        id: 19,
+        name: "Multicolor Acrylic Ganesh Shubh Labh Door Hanging",
+        category: "festive",
+        image: p19_img,
+        price: 449,
+    },
+    {
+        id: 20,
+        name: "Decorative Star Natural Wood Hanging Toran",
+        category: "festive",
+        image: p20_img,
+        price: 479,
+    },
+    {
+        id: 21,
+        name: "3 layer Candle Holder Stand",
+        category: "festive",
+        image: p21_img,
+        price: 3499,
+    },
+    {
+        id: 22,
+        name: "Multicolor Indian Dream Catcher",
+        category: "festive",
+        image: p22_img,
+        price: 1448,
+    },
+    {
+        id: 23,
+        name: "5pcs Gold Glass Hurricane Candle Holders",
+        category: "festive",
+        image: p23_img,
+        price: 4999,
+    },
+    {
+        id: 24,
+        name: "Radha Krishna Indian Metal Art Wall Hanging Handmade Showpiece",
+        category: "festive",
+        image: p24_img,
+        price: 3600,
+    },
+    {
+        id: 25,
+        name: "Framed Wall Paintings",
+        category: "painting",
+        image: p25_img,
+        price: 1999,
+    },
+    {
+        id: 26,
+        name: "Colorful Modern Abstract Canvas Painting for home",
+        category: "painting",
+        image: p26_img,
+        price: 999,
+    },
+    {
+        id: 27,
+        name: "Dancing Women Warli Art Wall Painting",
+        category: "painting",
+        image: p27_img,
+        price: 2999,
+    },
+    {
+        id: 28,
+        name: "Vibrant Buddha Canvas Painting for living room",
+        category: "painting",
+        image: p28_img,
+        price: 3999,
+    },
+    {
+        id: 29,
+        name: "Acrylic Strokes Painting for office",
+        category: "painting",
+        image: p29_img,
+        price: 999,
+    },
+    {
+        id: 30,
+        name: "Canvas Painting Beautiful Nature Art",
+        category: "painting",
+        image: p30_img,
+        price: 1999,
+    },
+    {
+        id: 31,
+        name: "Multicolored Abstract Painting",
+        category: "painting",
+        image: p31_img,
+        price: 4999,
+    },
+    {
+        id: 32,
+        name: "Beach Aspired Nature Painting for living room",
+        category: "painting",
+        image: p32_img,
+        price: 3999,
+    },
+    {
+        id: 33,
+        name: "Gold Glass & Iron Mirror Sets",
+        category: "mirror",
+        image: p33_img,
+        price: 1699,
+    },
+    {
+        id: 34,
+        name: "Black Metal Mirror Sets",
+        category: "mirror",
+        image: p34_img,
+        price: 1549,
+    },
+    {
+        id: 35,
+        name: "Peacock Blue And Red Acrylic Marble Finish Rangoli",
+        category: "festive",
+        image: p35_img,
+        price: 899,
+    },
+    {
+        id: 36,
+        name: "Gold Fabric Hanging Toran",
+        category: "festive",
+        image: p36_img,
+        price: 3999,
+    },
+    {
+        id: 37,
+        name: "Fluid Art Painting for living room",
+        category: "painting",
+        image: p37_img,
+        price: 1099,
+    },
+    {
+        id: 38,
+        name: "Waterfall Canvas Painting for living room",
+        category: "painting",
+        image: p38_img,
+        price: 3999,
+    },
+    {
+        id: 39,
+        name: "Saintpaulia Variegata",
+        category: "plants",
+        image: p39_img,
+        price: 2999,
+    },
+    {
+        id: 40,
+        name: "Echerevia Gifting Set",
+        category: "plants",
+        image: p40_img,
+        price: 3599,
+    },
+];
+
+export default all_product;
