6dfa204523188f3045797091537ad2de30bdf744
Priyanshi Sutariya <155957315+Priyanshisutariya04@users.noreply.github.com>
2024-04-08 12:21:05 +0530

    Add files via upload

---
+++ Pages/Shop.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Popular from "../Components/Popular/Popular";
+
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+
+const Shop = () => {
+  return (
+    <div>
+      <Popular />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <NewCollections />
+      <NewsLetter />
+    </div>
+  );
+};
+
+export default Shop;
