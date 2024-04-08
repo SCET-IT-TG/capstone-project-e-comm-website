97dc379a961eeeba9b609271ec35a49e5db0dd2f
JeeyaTandel <155955941+JeeyaTandel@users.noreply.github.com>
2024-04-08 12:07:47 +0530

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,38 @@
+import p1_img from './product_7.png'
+import p2_img from './product_13.png'
+import p3_img from './product_21.png'
+import p4_img from './product_28.png'
+
+let data_product = [
+  {
+    id: 7,
+    name: "Hildewintera Cristata",
+    category: "plants",
+    image: p1_img,
+    price: 2000,
+    
+  },
+  {
+    id: 13,
+    name: "Blue & White Glass Decorative Mirror",
+    category: "mirror",
+    image: p2_img,
+    price: 3200,
+  },
+  {
+    id: 21,
+        name: "3 layer Candle Holder Stand",
+        category: "festive",
+        image: p3_img,
+        price: 3499,
+  },
+  {
+    id: 28,
+        name: "Vibrant Buddha Canvas Painting for living room",
+        category: "painting",
+        image: p4_img,
+        price: 3999,
+  },
+];
+
+export default data_product;
